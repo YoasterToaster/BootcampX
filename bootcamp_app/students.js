87938e0a5e07cdd9d3d8ed1d6d5b58107b638827
YoasterToaster <joefoschia@gmail.com>
2023-12-14 13:35:39 -0500

    Add preventative measures for SQL injections to teachers.js and students.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,16 @@
 const { Pool } = require('pg');
-args = process.argv.slice(2);
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2
+  `;
 
 const pool = new Pool({
   user: 'vagrant',
@@ -7,14 +18,8 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-// console.log([args[0], args[1]]);
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
